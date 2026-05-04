f5e49592210fa10763be064a96361afae9a13d29
student2 <student2@usn.no>
2026-05-04 17:16:21 +0200

    Lagt til KategoriId i Kategori.cs

--- Models/Kategori.cs
+++ Models/Kategori.cs
@@ -9,7 +9,7 @@ public class Kategori
 
     [Required(ErrorMessage = "Navn er påkrevd for kategorier.")]
     [StringLength(100, ErrorMessage = "Kategorinavnet kan maks være 100 tegn")]
-    public string? KategoriNavn { get; set; } = string.Empty;
+    public string KategoriNavn { get; set; } = string.Empty;
 
     public ICollection<Bedrift> bedrifter { get; set; } = new List<Bedrift>();
 }
