cd2ffea3ec7e08eafb377f4ea9d9d6e431c0d367
Student2 <student2@usn.no>
2026-05-04 12:43:30 +0200

    Første commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TSD2491Gruppe25.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
